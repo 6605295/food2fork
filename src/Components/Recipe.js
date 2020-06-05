e6bb1c10167742f5760cbdc4cd057ddb42904588
unknown <alishah9946@gmail.com>
2020-06-05 12:49:02 +0500

    recipe responsive

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -12,33 +12,35 @@ class Recipe extends Component {
     } = this.props.Recipe;
     return (
       <div className="col-md-4 my-3">
-        <div className="card" style={{ width: "18rem" }}>
-          <img
-            className="card-img-top"
-            src={image_url}
-            alt="recipe"
-            style={{ width: "286px", height: "180px" }}
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">
-              <span className="text-warning">{publisher}</span>
-            </p>
+        <center>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              className="card-img-top"
+              src={image_url}
+              alt="recipe"
+              style={{ width: "286px", height: "180px" }}
+            />
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">
+                <span className="text-warning">{publisher}</span>
+              </p>
+            </div>
+            <div className="card-footer text-center">
+              <Link to={`/recipes/${recipe_id}`}>
+                <button className="btn btn-primary">Details</button>
+              </Link>
+              <a
+                href={source_url}
+                className="btn btn-success mx-3"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Recipe Url
+              </a>
+            </div>
           </div>
-          <div className="card-footer text-center">
-            <Link to={`/recipes/${recipe_id}`}>
-              <button className="btn btn-primary">Details</button>
-            </Link>
-            <a
-              href={source_url}
-              className="btn btn-success mx-3"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Recipe Url
-            </a>
-          </div>
-        </div>
+        </center>
       </div>
     );
   }
